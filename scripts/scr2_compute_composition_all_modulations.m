d03114fd04504ea906391080a6ef00c51c30f408
Francois Bertaux <francois.bertaux.2007@gmail.com>
2018-10-22 11:16:12 +0200

    scripts: now all compositions are computed by script 2

--- scripts/scr2_compute_composition_all_modulations.m
+++ scripts/scr2_compute_composition_all_modulations.m
@@ -7,6 +7,7 @@
 %
 %%%%%
 
+addpath('../model-code');
 addpath('../model-code/steady-state');
 
 
@@ -22,3 +23,16 @@ composition_scott = compute_cell_composition_from_growth_modulation(fitted_pars,
 % output 
 writetable(composition_scott, '../results-data/res2_cell-compositions/scott_2010_modulations.csv');
 
+%%% second, the basan data
+data_basan = readtable('../external-data/basan_2015_data.csv');
+% compute composition
+composition_basan = compute_cell_composition_from_growth_modulation(fitted_pars, data_basan);
+% output
+writetable(composition_basan, '../results-data/res2_cell-compositions/basan_2015_modulations.csv');
+
+%%% third, the si data
+data_si = readtable('../external-data/si_2017.csv');
+% compute composition
+composition_si = compute_cell_composition_from_growth_modulation(fitted_pars, data_si);
+% output
+writetable(composition_si, '../results-data/res2_cell-compositions/si_2017_modulations.csv');
