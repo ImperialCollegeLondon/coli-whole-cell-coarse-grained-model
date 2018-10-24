91c9c060fbb7b9e8bac27b3d178bd7a61ec3484f
Francois Bertaux <francois.bertaux.2007@gmail.com>
2018-10-24 14:14:55 +0200

    'working' script and figure plotting new figure 3

--- figure-plotting/figure_3.m
+++ figure-plotting/figure_3.m
@@ -2,41 +2,47 @@
 addpath('../utils-code/export_fig');
 
 
-%% load data and predictions
-exp_data = readtable('../results-data/basan-2015-si-2017-scale-normalized/nutrient_vs_cm_or_useless_modulation.csv');
-exp_data_no_cm = exp_data;
-exp_data_no_cm(exp_data_no_cm.cm_type > 0,:) = [];
-data_best_fit = readtable('../results-data/basan-2015-si-2017-fit/best_prediction.csv');
-all_predictions = cell(8,1);
-all_predictions_no_cm = cell(8,1);
-for i=1:8
-    all_predictions{i} = readtable(['../results-data/basan-2015-si-2017-fit/all-predictions/' num2str(i) '.csv']);
-    all_predictions_no_cm{i} = readtable(['../results-data/basan-2015-si-2017-fit/all-predictions_no-cm/' num2str(i) '.csv']);
-end
-
+%% load prediction data
+%
+best_pred_data = readtable('../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions/e_and_ra_over_r_data_fX-true/predictions.csv');
+best_pred_formula = fileread('../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions/e_and_ra_over_r_data_fX-true/formula.txt');
+%
+r_nut_useless_pred_data = readtable('../results-data/res4_basan-2015-si-2017-fit/single-sector-size-predictions/r_data_nut_useless_fX-true/predictions.csv');
+r_nut_useless_pred_formula = fileread('../results-data/res4_basan-2015-si-2017-fit/single-sector-size-predictions/r_data_nut_useless_fX-true/formula.txt');
+%
+e_nut_useless_pred_data = readtable('../results-data/res4_basan-2015-si-2017-fit/single-sector-size-predictions/e_data_nut_useless_fX-true/predictions.csv');
+e_nut_useless_pred_formula = fileread('../results-data/res4_basan-2015-si-2017-fit/single-sector-size-predictions/e_data_nut_useless_fX-true/formula.txt');
+%
+e_pred_data = readtable('../results-data/res4_basan-2015-si-2017-fit/single-sector-size-predictions/e_data_fX-true/predictions.csv');
+e_pred_formula = fileread('../results-data/res4_basan-2015-si-2017-fit/single-sector-size-predictions/e_data_fX-true/formula.txt');
+%
+e_and_r_pred_data = readtable('../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions/e_and_r_data_fX-true/predictions.csv');
+e_and_r_pred_formula = fileread('../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions/e_and_r_data_fX-true/formula.txt');
+%
+e_and_ra_pred_data = readtable('../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions/e_and_ra_data_fX-true/predictions.csv');
+e_and_ra_pred_formula = fileread('../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions/e_and_ra_data_fX-true/formula.txt');
 
 %% make the two size vs growth rate plots (data and best prediction)
 mk_size = 12;
-datas = {exp_data, data_best_fit};
 titles = {'Data', 'Prediction'};
+fields = {'real', 'prediction'};
+I_cm = find(best_pred_data.cm_type > 0);
+I_useless = find(best_pred_data.useless_type > 0);
+I_nut = find(best_pred_data.cm_type == 0 & best_pred_data.useless_type == 0);
 for i_data=1:2
     figure;
-    data = datas{i_data};
-    I_cm = find(data.cm_type > 0);
-    I_useless = find(data.useless_type > 0);
-    I_nut = find( data.cm_type == 0 & data.useless_type == 0);
-    hs(1) = plot(data.growth_rate_per_hr(I_nut), data.estimated_volume(I_nut), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
-    hs(2) = plot(data.growth_rate_per_hr(I_cm), data.estimated_volume(I_cm), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
-    hs(3) = plot(data.growth_rate_per_hr(I_useless), data.estimated_volume(I_useless), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
+    hs(1) = plot(best_pred_data.growth_rate_per_hr(I_nut), best_pred_data.(fields{i_data})(I_nut), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
+    hs(2) = plot(best_pred_data.growth_rate_per_hr(I_cm), best_pred_data.(fields{i_data})(I_cm), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
+    hs(3) = plot(best_pred_data.growth_rate_per_hr(I_useless), best_pred_data.(fields{i_data})(I_useless), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
     title(titles{i_data});
     xlabel('Growth rate (hr^{-1})');
     ylabel('Size');
-    ylim([0 12]); xlim([0 2]);
+    ylim([0 10]); xlim([0 2]);
     set(gca,'FontSize',25,'LineWidth',2);
     grid();
     legend(hs, ...
            {'nutrient (Basan 2015 and Si 2017)', 'chloramphenicol (Basan 2015 and Si 2017)', 'useless expression (Basan 2015)'}, ...
-           'FontSize',15,'LineWidth',1.5);
+           'FontSize',11,'LineWidth',1.0,'Location','North');
     set(gcf,'Color','w');
 end
 export_fig(figure(1),'../figure-assembly/figure-3-components/figure_3_panel_data.pdf');
@@ -46,56 +52,64 @@ export_fig(figure(2),'../figure-assembly/figure-3-components/figure_3_panel_pred
 
 
 
-%% make the single sector prediction plots
+%% display the first row (single sectors)
+datas = {r_nut_useless_pred_data, e_nut_useless_pred_data, e_pred_data};
+formulas = {r_nut_useless_pred_formula, e_nut_useless_pred_formula, e_pred_formula};
 figure;
 mk_size = 10;
-for i=1:4
-    data = all_predictions_no_cm{i};
+for i=1:3
+    data = datas{i};
+    subplot(1,3,i);
     I_cm = find(data.cm_type > 0);
     I_useless = find(data.useless_type > 0);
     I_nut = find( data.cm_type == 0 & data.useless_type == 0);
-    subplot(1,4,i);
-    plot(log(exp_data_no_cm.estimated_volume(I_nut)), log(data.estimated_volume(I_nut)), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
-%     plot(log(exp_data_no_cm.estimated_volume(I_cm)), log(data.estimated_volume(I_cm)), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
-    plot(log(exp_data_no_cm.estimated_volume(I_useless)), log(data.estimated_volume(I_useless)), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
+    plot(log(data.real(I_nut)), log(data.prediction(I_nut)), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
+    plot(log(data.real(I_useless)), log(data.prediction(I_useless)), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
+    plot(log(data.real(I_cm)), log(data.prediction(I_cm)), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
     plot([0 2.5], [0 2.5], 'Color', [1 1 1].*0.6, 'LineWidth', 1.5);
     set(gca,'FontSize',15,'LineWidth',1.5);
     xlabel('log real size');
     if i==1; ylabel('log predicted size'); end
     ylim([0 2.5]); xlim([0 2.5]);
-    title(data.formula_prediction{1},'FontSize',15);
+    title(formulas{i},'FontSize',15);
     axis square;
     grid();
     set(gca,'XTick',0:0.5:2.5,'YTick',0:0.5:2.5);
 end
 set(gcf,'Position',[0 0 1660 360],'Color','w');
 export_fig(gcf,'../figure-assembly/figure-3-components/figure_3_panel_single_sector.pdf');
+    
 
 
-%% make the double sector prediction plots
+%% display the second row (two sectors)
+datas = {e_and_r_pred_data, e_and_ra_pred_data, best_pred_data};
+formulas = {e_and_r_pred_formula, e_and_ra_pred_formula, best_pred_formula};
 figure;
 mk_size = 10;
-for i=5:8
-    data = all_predictions{i};
+for i=1:3
+    data = datas{i};
+    subplot(1,3,i);
     I_cm = find(data.cm_type > 0);
     I_useless = find(data.useless_type > 0);
     I_nut = find( data.cm_type == 0 & data.useless_type == 0);
-    subplot(1,4,i-4);
-    plot(log(exp_data.estimated_volume(I_nut)), log(data.estimated_volume(I_nut)), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
-    plot(log(exp_data.estimated_volume(I_cm)), log(data.estimated_volume(I_cm)), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
-    plot(log(exp_data.estimated_volume(I_useless)), log(data.estimated_volume(I_useless)), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
+    plot(log(data.real(I_nut)), log(data.prediction(I_nut)), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
+    plot(log(data.real(I_useless)), log(data.prediction(I_useless)), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
+    plot(log(data.real(I_cm)), log(data.prediction(I_cm)), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
     plot([0 2.5], [0 2.5], 'Color', [1 1 1].*0.6, 'LineWidth', 1.5);
     set(gca,'FontSize',15,'LineWidth',1.5);
     xlabel('log real size');
-    if i==5; ylabel('log predicted size'); end
+    if i==1; ylabel('log predicted size'); end
     ylim([0 2.5]); xlim([0 2.5]);
-    title(data.formula_prediction{1},'FontSize',15);
+    title(formulas{i},'FontSize',15);
     axis square;
     grid();
     set(gca,'XTick',0:0.5:2.5,'YTick',0:0.5:2.5);
 end
 set(gcf,'Position',[0 0 1660 360],'Color','w');
 export_fig(gcf,'../figure-assembly/figure-3-components/figure_3_panel_two_sectors.pdf');
+
+
+
 
 
 %%
--- scripts/scr4_predict_size_from_composition.m
+++ scripts/scr4_predict_size_from_composition.m
@@ -21,7 +21,7 @@ for sector=single_sectors
         for fX_str={'true','false'}
             % compute the size regression for this sector / data group / fX
             % or direct volume
-            eval(strjoin(["[R2, scale_factor, exponents, predictions] = explain_size_from_composition(" data_str{1} ", 'cell_size', {'" sector{1} "'}, " fX_str{1} ");"],''));
+            eval(strjoin(["[R2, scale_factor, exponents, prediction] = explain_size_from_composition(" data_str{1} ", 'cell_size', {'" sector{1} "'}, " fX_str{1} ");"],''));
             eval(strjoin([sector{1} "(end+1,1) = R2;"],''));
             % for automatic table creation...
             if strcmp(sector{1},'e')
@@ -34,10 +34,20 @@ for sector=single_sectors
             writetable(table(scale_factor),[out_dir '/scale_factor.csv']);
             writetable(table(exponents),[out_dir '/exponents.csv']);
             eval(strjoin(["real = " data_str{1} ".cell_size;"],''));
+            eval(strjoin(["growth_rate_per_hr = " data_str{1} ".growth_rate_per_hr;"],''));
             eval(strjoin(["nutrient_type = " data_str{1} ".nutrient_type;"],''));
             eval(strjoin(["cm_type = " data_str{1} ".cm_type;"],''));
             eval(strjoin(["useless_type = " data_str{1} ".useless_type;"],''));
-            writetable(table(predictions,real,nutrient_type,cm_type,useless_type),[out_dir '/predictions.csv']);
+            writetable(table(growth_rate_per_hr,real,prediction,nutrient_type,cm_type,useless_type),[out_dir '/predictions.csv']);
+            if strcmp(fX_str,'true')
+                formula_str = strjoin(["f_X \\propto " sector{1} "^{" num2str(round(100*exponents(1))/100) '}'],'');
+            else
+                formula_str = strjoin(["V_{div} \\propto " sector{1} "^{" num2str(round(100*exponents(1))/100) '}'],'');
+            end
+            formula_str = strrep(formula_str,'ra_over_r','(r_a/r)');
+            fileID = fopen([out_dir '/formula.txt'],'w');
+            fprintf(fileID,formula_str);
+            fclose(fileID);
         end
     end
     table_str = strjoin([table_str sector{1} ','],''); % for automatic table creation
@@ -59,7 +69,7 @@ for i_sector_1=1:length(single_sectors)
         eval(strjoin([sector_1{1} "_and_" sector_2{1} " = [];"],''));
         for data_str={'data_nut_cm','data_nut_useless','data_nut_only','data'}
             for fX_str={'true','false'}
-                eval(strjoin(["[R2, scale_factor, exponents, predictions] = explain_size_from_composition(" data_str{1} ", 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, " fX_str{1} ");"],''));
+                eval(strjoin(["[R2, scale_factor, exponents, prediction] = explain_size_from_composition(" data_str{1} ", 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, " fX_str{1} ");"],''));
                 eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = R2;"],''));
                 if strcmp(strjoin([sector_1{1} "_and_" sector_2{1}],''),'e_and_r')
                     table_row_str = strjoin([table_row_str "'" data_str{1} "_fX-" fX_str{1} "',"],'');
@@ -71,10 +81,20 @@ for i_sector_1=1:length(single_sectors)
                 writetable(table(scale_factor),[out_dir '/scale_factor.csv']);
                 writetable(table(exponents),[out_dir '/exponents.csv']);
                 eval(strjoin(["real = " data_str{1} ".cell_size;"],''));
+                eval(strjoin(["growth_rate_per_hr = " data_str{1} ".growth_rate_per_hr;"],''));
                 eval(strjoin(["nutrient_type = " data_str{1} ".nutrient_type;"],''));
                 eval(strjoin(["cm_type = " data_str{1} ".cm_type;"],''));
                 eval(strjoin(["useless_type = " data_str{1} ".useless_type;"],''));
-                writetable(table(predictions,real,nutrient_type,cm_type,useless_type),[out_dir '/predictions.csv']);
+                writetable(table(growth_rate_per_hr,real,prediction,nutrient_type,cm_type,useless_type),[out_dir '/predictions.csv']);
+                if strcmp(fX_str,'true')
+                    formula_str = strjoin(["f_X \\propto " sector_1{1} "^{" num2str(round(100*exponents(1))/100) "} \\times " sector_2{1} "^{" num2str(round(100*exponents(2))/100) '}'],'');
+                else
+                    formula_str = strjoin(["V_{div} \\propto " sector{1} "^{" num2str(round(100*exponents(1))/100) "} \\times " sector_2{1} "^{" num2str(round(100*exponents(2))/100) '}'],'');  
+                end
+                formula_str = strrep(formula_str,'ra_over_r','(r_a/r)');
+                fileID = fopen([out_dir '/formula.txt'],'w');
+                fprintf(fileID,formula_str);
+                fclose(fileID);
             end
         end
         table_str = strjoin([table_str sector_1{1} "_and_" sector_2{1} ','],'');
@@ -85,48 +105,5 @@ table_creation_str = strjoin([table_str table_row_str],'');
 eval(table_creation_str);
 writetable(results,'../results-data/res4_basan-2015-si-2017-fit/two-sectors-size-predictions-R2-values.csv','WriteRowNames',true);
 
-
-
-
-% 
-% 
-% for i_sector_1=1:length(single_sectors)
-%     for i_sector_2=i_sector_1+1:length(single_sectors)
-%         sector_1 = {single_sectors{i_sector_1}};
-%         sector_2 = {single_sectors{i_sector_2}};
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} " = [];"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data_nut_only, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, true);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data_nut_cm, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, true);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data_nut_useless, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, true);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, true);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data_nut_only, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, false);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data_nut_cm, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, false);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data_nut_useless, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, false);"],''));
-%         eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = explain_size_from_composition(data, 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, false);"],''));
-%     end
-% end
-% results = table(e_and_r,e_and_ra,e_and_ra_over_r,e_and_fE,e_and_fR,e_and_fQ,e_and_a,...
-%     r_and_ra,r_and_ra_over_r,r_and_fE,r_and_fR,r_and_fQ,r_and_a,...
-%     ra_and_ra_over_r,ra_and_fE,ra_and_fR,ra_and_fQ,ra_and_a,...
-%     ra_over_r_and_fE,ra_over_r_and_fR,ra_over_r_and_fQ,ra_over_r_and_a,...
-%     fE_and_fR,fE_and_fQ,fE_and_a,...
-%     fR_and_fQ,fR_and_a,...
-%     fQ_and_a,...
-%     'RowNames',...
-%     {'only_nut_vol_via_fX','nut_cm_vol_via_fX','nut_useless_vol_via_fX','all_vol_via_fX', ...
-%     'only_nut_vol_direct','nut_cm_vol_direct','nut_useless_vol_direct','all_vol_direct'});
-
-
-
-
-
-
-
-%%% select interesting
-% [nut_only_e_vol_direct.R2, nut_only_e_vol_direct.scale, nut_only_e_vol_direct.exponents, nut_only_e_vol_direct.predictions] = explain_size_from_composition(data_nut_only, 'cell_size', {'e'}, false);
-% [nut_only_e_vol_via_fX.R2, nut_only_e_vol_via_fX.scale, nut_only_e_vol_via_fX.exponents, nut_only_e_vol_via_fX.predictions] = explain_size_from_composition(data_nut_only, 'cell_size', {'e'}, true);
-%
-% [all_e_ra_over_r_vol_direct.R2, all_e_ra_over_r_vol_direct.scale, all_e_ra_over_r_vol_direct.exponents, all_e_ra_over_r_vol_direct.predictions] = explain_size_from_composition(data, 'cell_size', {'e', 'ra_over_r'}, false);
-% [all_e_ra_over_r_vol_via_fX.R2, all_e_ra_over_r_vol_via_fX.scale, all_e_ra_over_r_vol_via_fX.exponents, all_e_ra_over_r_vol_via_fX.predictions] = explain_size_from_composition(data, 'cell_size', {'e', 'ra_over_r'}, true);
 
 
