51b8ba6985fe6d973ea463fa7ae392550c95b5ff
fbertaux <fbertaux@MacBook-Pro-de-fbertaux.local>
2018-11-18 22:11:11 +0100

    sup figure 6 size pred volume direct

---
+++ figure-plotting/sup_figure_6.m
@@ -0,0 +1,39 @@
+
+%%%
+
+
+%
+two_sec_preds = readtable('../results-data/res4_basan-2015-si-2017-taheri-2015-fit/two-sectors-size-predictions-R2-values_ref.csv','ReadRowNames',true);
+two_sec_preds = two_sec_preds('data_fX-false',:);
+[~,i_best_pred] = max(two_sec_preds{:,:});
+best_pred = two_sec_preds(:,i_best_pred);
+best_pred_name = best_pred.Properties.VariableNames{1};
+
+
+%
+pred_data = readtable(['../results-data/res4_basan-2015-si-2017-taheri-2015-fit/ref/two-sectors-size-predictions/' best_pred_name '_data_fX-false/predictions.csv']);
+formula = fileread(['../results-data/res4_basan-2015-si-2017-taheri-2015-fit/ref/two-sectors-size-predictions/' best_pred_name '_data_fX-false/formula.txt']);
+
+%
+mk_size = 10;
+I_cm = find(pred_data.cm_type > 0);
+I_useless = find(pred_data.useless_type > 0);
+I_nut = find( pred_data.cm_type == 0 & pred_data.useless_type == 0);
+plot(log(pred_data.real(I_nut)), log(pred_data.prediction(I_nut)), 'go', 'MarkerSize', mk_size, 'MarkerFaceColor', 'g'); hold on;
+plot(log(pred_data.real(I_useless)), log(pred_data.prediction(I_useless)), 'r^', 'MarkerSize', mk_size, 'MarkerFaceColor', 'r'); hold on;
+plot(log(pred_data.real(I_cm)), log(pred_data.prediction(I_cm)), 'bs', 'MarkerSize', mk_size, 'MarkerFaceColor', 'b'); hold on;
+plot([0 2.5], [0 2.5], 'Color', [1 1 1].*0.6, 'LineWidth', 1.5);
+set(gca,'FontSize',15,'LineWidth',1.5);
+xlabel('log real size');
+ylabel('log predicted size');
+ylim([0 3]); xlim([0 3]);
+title(formula,'FontSize',15);
+axis square;
+set(gca,'XTick',0:0.5:2.5,'YTick',0:0.5:2.5);
+
+
+%
+addpath('../utils-code/export_fig');
+set(gcf,'Position',[270 461 694 517],'Color','w');
+export_fig(gcf,'../figure-assembly/sup-figure-6-components/sup_figure_6_all.pdf');
+close;
--- scripts/scr4_predict_size_from_composition.m
+++ scripts/scr4_predict_size_from_composition.m
@@ -18,20 +18,20 @@ for i_m=1:length(model_names)
     %
     single_sectors = {'e', 'r', 'ra', 'ra_over_r', 'fE', 'fR', 'fQ', 'a'};
     % for automatic table creation...
-    table_str = "results = table(";
-    table_row_str = "'RowNames',{";
+    table_str = 'results = table(';
+    table_row_str = '''RowNames'',{';
     % iterate on sectors x data group x via fX or direct size prediction
     for sector=single_sectors
-        eval(strjoin([sector{1} " = [];"],'')); % for automatic table creation...
+        eval([sector{1} ' = [];']); % for automatic table creation...
         for data_str={'data_nut_cm','data_nut_useless','data_nut_only','data'}
             for fX_str={'true','false'}
                 % compute the size regression for this sector / data group / fX
                 % or direct volume
-                eval(strjoin(["[R2, scale_factor, exponents, prediction] = explain_size_from_composition(" data_str{1} ", 'cell_size', {'" sector{1} "'}, " fX_str{1} ");"],''));
-                eval(strjoin([sector{1} "(end+1,1) = R2;"],''));
+                eval(['[R2, scale_factor, exponents, prediction] = explain_size_from_composition(' data_str{1} ', ''cell_size'', {''' sector{1} '''}, ' fX_str{1} ');']);
+                eval([sector{1} '(end+1,1) = R2;']);
                 % for automatic table creation...
                 if strcmp(sector{1},'e')
-                    table_row_str = strjoin([table_row_str "'" data_str{1} "_fX-" fX_str{1} "',"],'');
+                    table_row_str = [table_row_str '''' data_str{1} '_fX-' fX_str{1} ''','];
                 end
                 % make and write tables with the regression parameters and the
                 % predictions
@@ -39,16 +39,16 @@ for i_m=1:length(model_names)
                 mkdir(out_dir);
                 writetable(table(scale_factor),[out_dir '/scale_factor.csv']);
                 writetable(table(exponents),[out_dir '/exponents.csv']);
-                eval(strjoin(["real = " data_str{1} ".cell_size;"],''));
-                eval(strjoin(["growth_rate_per_hr = " data_str{1} ".growth_rate_per_hr;"],''));
-                eval(strjoin(["nutrient_type = " data_str{1} ".nutrient_type;"],''));
-                eval(strjoin(["cm_type = " data_str{1} ".cm_type;"],''));
-                eval(strjoin(["useless_type = " data_str{1} ".useless_type;"],''));
+                eval(['real = ' data_str{1} '.cell_size;']);
+                eval(['growth_rate_per_hr = ' data_str{1} '.growth_rate_per_hr;']);
+                eval(['nutrient_type = ' data_str{1} '.nutrient_type;']);
+                eval(['cm_type = ' data_str{1} '.cm_type;']);
+                eval(['useless_type = ' data_str{1} '.useless_type;']);
                 writetable(table(growth_rate_per_hr,real,prediction,nutrient_type,cm_type,useless_type),[out_dir '/predictions.csv']);
                 if strcmp(fX_str,'true')
-                    formula_str = strjoin(["f_X \\propto " sector{1} "^{" num2str(round(100*exponents(1))/100) '}'],'');
+                    formula_str = ['f_X \\propto ' sector{1} '^{' num2str(round(100*exponents(1))/100) '}'];
                 else
-                    formula_str = strjoin(["V_{div} \\propto " sector{1} "^{" num2str(round(100*exponents(1))/100) '}'],'');
+                    formula_str = ['V_{div} \\propto ' sector{1} '^{' num2str(round(100*exponents(1))/100) '}'];
                 end
                 formula_str = strrep(formula_str,'ra_over_r','(r_a/r)');
                 fileID = fopen([out_dir '/formula.txt'],'w');
@@ -56,29 +56,29 @@ for i_m=1:length(model_names)
                 fclose(fileID);
             end
         end
-        table_str = strjoin([table_str sector{1} ','],''); % for automatic table creation
+        table_str = [table_str sector{1} ',']; % for automatic table creation
     end
     % create and write the table
-    table_row_str = strjoin([table_row_str{1}(1:end-1) "});"],'');
-    table_creation_str = strjoin([table_str table_row_str],'');
+    table_row_str = [table_row_str(1:end-1) '});'];
+    table_creation_str = [table_str table_row_str];
     eval(table_creation_str);
     writetable(results,['../results-data/res4_basan-2015-si-2017-taheri-2015-fit/single-sector-size-predictions-R2-values_' model_names{i_m}  '.csv'],'WriteRowNames',true);
     
     
     %%% do two sectors prediction
-    table_str = "results = table(";
-    table_row_str = "'RowNames',{";
+    table_str = 'results = table(';
+    table_row_str = '''RowNames'',{';
     for i_sector_1=1:length(single_sectors)
         for i_sector_2=i_sector_1+1:length(single_sectors)
             sector_1 = {single_sectors{i_sector_1}};
             sector_2 = {single_sectors{i_sector_2}};
-            eval(strjoin([sector_1{1} "_and_" sector_2{1} " = [];"],''));
+            eval([sector_1{1} '_and_' sector_2{1} ' = [];']);
             for data_str={'data_nut_cm','data_nut_useless','data_nut_only','data'}
                 for fX_str={'true','false'}
-                    eval(strjoin(["[R2, scale_factor, exponents, prediction] = explain_size_from_composition(" data_str{1} ", 'cell_size', {'" sector_1{1} "','" sector_2{1} "'}, " fX_str{1} ");"],''));
-                    eval(strjoin([sector_1{1} "_and_" sector_2{1} "(end+1,1) = R2;"],''));
-                    if strcmp(strjoin([sector_1{1} "_and_" sector_2{1}],''),'e_and_r')
-                        table_row_str = strjoin([table_row_str "'" data_str{1} "_fX-" fX_str{1} "',"],'');
+                    eval(['[R2, scale_factor, exponents, prediction] = explain_size_from_composition(' data_str{1} ', ''cell_size'', {''' sector_1{1} ''',''' sector_2{1} '''}, ' fX_str{1} ');']);
+                    eval([sector_1{1} '_and_' sector_2{1} '(end+1,1) = R2;']);
+                    if strcmp([sector_1{1} '_and_' sector_2{1}],'e_and_r')
+                        table_row_str = [table_row_str '''' data_str{1} '_fX-' fX_str{1} ''','];
                     end
                     % make and write tables with the regression parameters and the
                     % predictions
@@ -86,16 +86,16 @@ for i_m=1:length(model_names)
                     mkdir(out_dir);
                     writetable(table(scale_factor),[out_dir '/scale_factor.csv']);
                     writetable(table(exponents),[out_dir '/exponents.csv']);
-                    eval(strjoin(["real = " data_str{1} ".cell_size;"],''));
-                    eval(strjoin(["growth_rate_per_hr = " data_str{1} ".growth_rate_per_hr;"],''));
-                    eval(strjoin(["nutrient_type = " data_str{1} ".nutrient_type;"],''));
-                    eval(strjoin(["cm_type = " data_str{1} ".cm_type;"],''));
-                    eval(strjoin(["useless_type = " data_str{1} ".useless_type;"],''));
+                    eval(['real = ' data_str{1} '.cell_size;']);
+                    eval(['growth_rate_per_hr = ' data_str{1} '.growth_rate_per_hr;']);
+                    eval(['nutrient_type = ' data_str{1} '.nutrient_type;']);
+                    eval(['cm_type = ' data_str{1} '.cm_type;']);
+                    eval(['useless_type = ' data_str{1} '.useless_type;']);
                     writetable(table(growth_rate_per_hr,real,prediction,nutrient_type,cm_type,useless_type),[out_dir '/predictions.csv']);
                     if strcmp(fX_str,'true')
-                        formula_str = strjoin(["f_X \\propto " sector_1{1} "^{" num2str(round(100*exponents(1))/100) "} \\times " sector_2{1} "^{" num2str(round(100*exponents(2))/100) '}'],'');
+                        formula_str = ['f_X \\propto ' sector_1{1} '^{' num2str(round(100*exponents(1))/100) '} \\times ' sector_2{1} '^{' num2str(round(100*exponents(2))/100) '}'];
                     else
-                        formula_str = strjoin(["V_{div} \\propto " sector{1} "^{" num2str(round(100*exponents(1))/100) "} \\times " sector_2{1} "^{" num2str(round(100*exponents(2))/100) '}'],'');
+                        formula_str = ['V_{div} \\propto ' sector_1{1} '^{' num2str(round(100*exponents(1))/100) '} \\times ' sector_2{1} '^{' num2str(round(100*exponents(2))/100) '}'];
                     end
                     formula_str = strrep(formula_str,'ra_over_r','(r_a/r)');
                     fileID = fopen([out_dir '/formula.txt'],'w');
@@ -103,11 +103,11 @@ for i_m=1:length(model_names)
                     fclose(fileID);
                 end
             end
-            table_str = strjoin([table_str sector_1{1} "_and_" sector_2{1} ','],'');
+            table_str = [table_str sector_1{1} '_and_' sector_2{1} ','];
         end
     end
-    table_row_str = strjoin([table_row_str{1}(1:end-1) "});"],'');
-    table_creation_str = strjoin([table_str table_row_str],'');
+    table_row_str = [table_row_str(1:end-1) '});'];
+    table_creation_str = [table_str table_row_str];
     eval(table_creation_str);
     writetable(results,['../results-data/res4_basan-2015-si-2017-taheri-2015-fit/two-sectors-size-predictions-R2-values_' model_names{i_m} '.csv'],'WriteRowNames',true);
     
