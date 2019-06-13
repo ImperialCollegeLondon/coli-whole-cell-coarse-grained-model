a56b82078f31b9acbf35022ea4c4f94f56538c2e
Francois Bertaux <francois.bertaux.2007@gmail.com>
2019-06-13 10:38:37 +0200

    working on Dai et al.

---
+++ scripts/scrXX_fit_model_to_scott_data_fR_a_ratio.m
@@ -0,0 +1,152 @@
+
+%%%%%
+% Data to fit is scott data where ribosomal fraction is measured (nutrient
+% and chloramphenicol 2D
+%
+% Three parameters are fitted: sigma, a_sat, q, fR_a_ratio
+%
+% Parameter space is explored using a grid-like approach (Sobol sequence).
+%
+% For each point, we simulate the scott nutrient x chloramphenicol
+% modulation and compute the mean square error between model and data
+% ribosomal fraction
+%
+% we don't explore points for which the maximum growth rate (k->infinity)
+% is less that 15 minutes doubling time
+%
+%%%%%
+
+function comp_data = scrXX_fit_model_to_scott_data_fR_a_ratio()
+
+%%% various paths needed
+addpath('../utils-code'); % for sobol
+addpath('../model-code');
+addpath('../model-code/steady-state'); % to compute optimal allocation for each condition
+
+%%% parameters of the sobol exploration
+log_sigma_min = -1;
+q_min = 0.3; q_max = 0.7;
+a_sats = [0.001, 0.002, 0.005, 0.01, ...
+    0.02, 0.05, 0.1, 0.2, ...
+    0.5, 1.0, 2.0, 5.0];
+log_sigma_max = log(200) .* ones(size(a_sats));
+minimum_max_alpha = log(2)/0.25; % 15 minutes doubling time
+
+%%% perform sobol exploration
+for i_a = 1:length(a_sats)
+    file_path = ['../results-data/resXX_scott-2010-fit_fR_a_ratio/sobol-exploration_asat-' num2str(a_sats(i_a)) '.mat'];
+    do_sobol_explo(file_path, 50, 2000, @(x)(simulate_scott_fR_a_ratio(x, minimum_max_alpha)), [log_sigma_min, a_sats(i_a), q_min], [log_sigma_max(i_a), a_sats(i_a), q_max]);
+end
+
+%%% compute cost and save as csv file
+for i_a = 1:length(a_sats)
+    file_path = ['../results-data/resXX_scott-2010-fit_fR_a_ratio/sobol-exploration_asat-' num2str(a_sats(i_a)) '.mat'];
+    explo = load(file_path);
+    explo_csv_with_cost = sobol_to_csv_with_cost(explo.explo.data);
+    explo_csv_with_cost(isnan(explo_csv_with_cost.goodness_fit),:) = [];
+    writetable(explo_csv_with_cost, ['../results-data/resXX_scott-2010-fit_fR_a_ratio/sobol-exploration-with-cost_asat-' num2str(a_sats(i_a)) '.csv']);
+end
+
+%%% load back, extract
+cost_best_sobol = zeros(length(a_sats),1); sigma_best_sobol = zeros(length(a_sats),1); q_best_sobol = zeros(length(a_sats),1);
+cost_best_local = zeros(length(a_sats),1); sigma_best_local = zeros(length(a_sats),1); q_best_local = zeros(length(a_sats),1);
+for i_a = 1:length(a_sats)
+    data = readtable(['../results-data/resXX_scott-2010-fit_fR_a_ratio/sobol-exploration-with-cost_asat-' num2str(a_sats(i_a)) '.csv']);
+    [cost_best_sobol(i_a), I] = min(data.log_goodness_fit);
+    sigma_best_sobol(i_a) = data.sigma(I);
+    q_best_sobol(i_a) = data.q(I);
+    % start local search from this best (a_sat still fixed) except if done
+    % already
+    if ~exist('../results-data/resXX_scott-2010-fit_fR_a_ratio/summary-best-pars.csv','file')
+        x_local_best = fminsearch(@(x)(simulate_scott_and_compute_cost_asat_fixed(x,a_sats(i_a),minimum_max_alpha)),[sigma_best_sobol(i_a), q_best_sobol(i_a)]);
+        cost_best_local(i_a) = log(simulate_scott_and_compute_cost_asat_fixed(x_local_best, a_sats(i_a), minimum_max_alpha));
+        sigma_best_local(i_a) = x_local_best(1);
+        q_best_local(i_a) = x_local_best(2);
+    else
+        scott_fit_best_pars = readtable('../results-data/resXX_scott-2010-fit_fR_a_ratio/summary-best-pars.csv');
+        cost_best_local = scott_fit_best_pars.cost_best_local;
+        sigma_best_local = scott_fit_best_pars.sigma_best_local;
+        q_best_local = scott_fit_best_pars.q_best_local;
+    end
+end
+
+%%% write in table if not done
+if ~exist('../results-data/resXX_scott-2010-fit_fR_a_ratio/summary-best-pars.csv','file')
+    a_sat = a_sats';
+    scott_fit_best_pars = table(a_sat, sigma_best_sobol, q_best_sobol, cost_best_sobol, sigma_best_local, q_best_local, cost_best_local);
+    writetable(scott_fit_best_pars, '../results-data/resXX_scott-2010-fit_fR_a_ratio/summary-best-pars.csv');
+end
+
+%%% also compute the composition for scott data of all local best and write
+%%% it
+for i_a = 1:length(a_sats)
+    pars.sigma = sigma_best_local(i_a);
+    pars.a_sat = a_sats(i_a);
+    pars.q = q_best_local(i_a);
+    mod_data = readtable('../external-data/scott_2010_data.csv');
+    mod_data = mod_data(mod_data.useless_type==0, :);
+    composition_data = compute_cell_composition_from_growth_modulation(pars, mod_data);
+    writetable(composition_data,['../results-data/resXX_scott-2010-fit_fR_a_ratio/scott-predictions_local-fixed-asat-fit_asat-' num2str(a_sats(i_a)) '.csv']);
+end
+
+
+end
+
+
+function composition_data = simulate_scott_fR_a_ratio(x, minimum_max_alpha)
+% order: sigma(!log!), a_sat, q
+pars.sigma = exp(x(1));
+pars.a_sat = x(2);
+pars.q = x(3);
+pars.fR_a_ratio = 0.11 * 0.76 / pars.a_sat; % because of Dai et al. MM relationship btw elongation rate and R/P !!!
+cell_pars.biophysical.sigma = pars.sigma; 
+cell_pars.biophysical.a_sat = pars.a_sat; 
+cell_pars.constraint.q = pars.q;
+cell_pars.constraint.fR_a_ratio = pars.fR_a_ratio;
+% check if max growth rate high enough, otherwise don't do sim and return
+% empty array
+max_alpha = give_alpha_max(cell_pars); % MAYBE WRONG WITH fR_a model variant ?
+if max_alpha < minimum_max_alpha
+    composition_data = [];
+    return;
+end
+% scott nut x cm data
+mod_data = readtable('../external-data/scott_2010_data.csv');
+mod_data = mod_data( mod_data.useless_type==0, :);
+composition_data = compute_cell_composition_from_growth_modulation_fR_a_ratio(pars, mod_data);
+end
+
+function sobol_csv = sobol_to_csv_with_cost(d)
+sigma = zeros(length(d),1);
+a_sat = zeros(length(d),1);
+q = zeros(length(d),1);
+goodness_fit = nan(length(d),1);
+log_goodness_fit = nan(length(d),1);
+a_max = nan(length(d),1);
+for i=1:length(d)
+    sigma(i) = exp(d(i).x(1)); % ! log !
+    a_sat(i) = d(i).x(2);
+    q(i) = d(i).x(3);
+    sim_scott = d(i).fx;
+    if ~isempty(sim_scott)
+        delta_fR_vec = sim_scott.estim_ribosomal_fraction - sim_scott.model_fR;
+        goodness_fit(i) = mean(delta_fR_vec.^2);
+        log_goodness_fit(i) = log(goodness_fit(i));
+        a_max(i) = max(sim_scott.model_a);
+    end
+end
+sobol_csv = table(sigma,a_sat,q,goodness_fit,log_goodness_fit,a_max);
+end
+
+function cost = simulate_scott_and_compute_cost_asat_fixed(x, a_sat, minimum_max_alpha)
+xx = [x(1), a_sat, x(2)]
+xx(1) = log(xx(1)); % because now sigma in log
+comp_data = simulate_scott_fR_a_ratio(xx, minimum_max_alpha);
+if isempty(comp_data)
+    cost = 1e300;
+    return;
+end
+delta_fR_vec = comp_data.estim_ribosomal_fraction - comp_data.model_fR;
+cost = mean(delta_fR_vec.^2);
+log(cost)
+end
--- scripts/scrXX_study_size_dai_inact.m
+++ scripts/scrXX_study_size_dai_inact.m
@@ -1,5 +1,5 @@
 
-% empirical inactive proteome fraction vs growth rate + fit
+% empirical active proteome fraction vs growth rate + fit
 dai_data = readtable('../external-data/dai_2016_data.csv');
 dai_fit_fun = @(x) 0.9408422 + (0.1557039 - 0.9408422)./(1 + (x./0.1949719).^1.957422);
 gr_vec = linspace(0,2,100);
@@ -7,27 +7,31 @@ dai_fit_vec = dai_fit_fun(gr_vec);
 subplot(2,2,1);
 plot(dai_data.growth_rate_per_hr, dai_data.active_ribosome_fraction, 'ko'); hold on;
 plot(gr_vec, dai_fit_vec, 'r');
+ylabel('Active ribosome fraction');
+xlabel('Growth rate (hr^{-1})');
+title('Dai et al. Fig 3c and empirical fit');
 
 % formula best fit
-fitted_size_pars = readtable('../results-data/res4_basan-2015-si-2017-taheri-2015-fit/ref/two-sectors-size-predictions/e_and_ra_over_r_data_fX-true/exponents.csv');      
-
-% compute compo
-compos = readtable('../results-data/res3_cell-compositions/basan_2015_si_2017_taheri_2015_modulations.csv');
-compos(compos.useless_type > 0,:) = [];
-compos(compos.cm_type > 0,:) = [];
+fitted_size_pars = readtable('../results-data/res4_basan-2015-si-2017-taheri-2015-fit/ref/e_and_ra_over_r_data_fX-true/exponents.csv');
+predictions_best_fit = readtable('../results-data/res4_basan-2015-si-2017-taheri-2015-fit/ref/e_and_ra_over_r_data_fX-true/predictions.csv');
+predictions_best_fit(predictions_best_fit.useless_type > 0,:) = [];
+predictions_best_fit(predictions_best_fit.cm_type > 0,:) = [];
 
 %
-compos.model_ra_over_r = dai_fit_fun(compos.growth_rate_per_hr);
-pred_size_ratio_one = 1 ./ compos.model_e.^1 ./ (1-compos.model_a);
-pred_size = 1 ./ (compos.model_e.^1 .* compos.model_ra_over_r.^(-2/3)) ./ (1-compos.model_a);
+empirical_ra_over_r = dai_fit_fun(predictions_best_fit.growth_rate_per_hr);
+predictions_size_ratio_non_one = predictions_best_fit.prediction .* empirical_ra_over_r.^(-fitted_size_pars.exponents(2));
 
 
 subplot(2,2,2);
-plot( log(compos.cell_size) , log(pred_size_ratio_one) , 'go'); hold on;
-pfit_1 = polyfit(log(compos.cell_size), log(pred_size_ratio_one), 1);
-plot( [log(0.8) log(10)], polyval(pfit_1,[log(0.8) log(10)]),'k');
+plot( log(predictions_best_fit.real) , log(predictions_best_fit.prediction) , 'go'); hold on;
+plot( [0 3], [0 3],'k');
+title('Size prediction with active ribosome ratio equal 1');
+ylabel('log measured size'); xlabel('log predicted size');
+
 
 subplot(2,2,4);
-plot( log(compos.cell_size) , log(pred_size) , 'gs'); hold on;
-pfit_2 = polyfit(log(compos.cell_size), log(pred_size), 1);
-plot( [log(0.8) log(10)], polyval(pfit_2,[log(0.8) log(10)]),'k');
+plot( log(predictions_best_fit.real) , log(predictions_size_ratio_non_one) , 'gs'); hold on;
+plot( [0 3], [0 3],'k');
+title('Size prediction with active ribosome ratio empirical from Dai');
+ylabel('log measured size'); xlabel('log predicted size');
+
