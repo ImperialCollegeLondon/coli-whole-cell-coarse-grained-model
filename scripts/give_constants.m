8090776df962838cf38cd32bcc9b6fc760c4504c
fbertaux <francois.bertaux@pasteur.fr>
2019-09-28 15:24:29 +0200

    looked at cm koff

--- scripts/give_constants.m
+++ scripts/give_constants.m
@@ -4,7 +4,7 @@ constants.K = 0.11 * 0.76; % in units of extended ribosome mass fraction. from D
 
 constants.sigma = 1 / (7336 * 1.67 / 22) * 3600; % in hr-1. from Dai et al max elong rate (22 aa/s), and extended ribosome AA number (7336 * 1.67)
 
-constants.cm_koff = 0.01; %5.04; % in hr-1. From Dai et al. SI (0.084 min-1), itself referencing Harvey et al., Antimicrob Agents Chemother, 1980
+constants.cm_koff = 0.1; %5.04; % in hr-1. From Dai et al. SI (0.084 min-1), itself referencing Harvey et al., Antimicrob Agents Chemother, 1980
 
 %constants.cm_kon_per_uM = 2.04; % in hr-1 per uM. From Dai et al. SI (0.034 per min per uM), itself referencing Harvey et al., Antimicrob Agents Chemother, 1980
 
